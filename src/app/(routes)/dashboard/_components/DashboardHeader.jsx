d12ce7b6f32ee575e015bebd620479c2eafc53ff
samarthsb4real <samarthbhadane23@gmail.com>
2024-07-27 11:34:18 +0530

    dashboard layout

---
+++ src/app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -0,0 +1,15 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+function DashboardHeader() {
+  return (
+    <div className="p-5 shadow-sm border-b flex justify-between">
+      <div></div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  );
+}
+
+export default DashboardHeader;
--- src/app/(routes)/dashboard/layout.jsx
+++ src/app/(routes)/dashboard/layout.jsx
@@ -1,13 +1,16 @@
 "use client";
 import React, { useEffect } from "react";
 import Sidebar from "./_components/Sidebar";
+import DashboardHeader from "./_components/DashboardHeader";
 
 export default function DashboardLayout({ children }) {
   return (
     <div>
+      <DashboardHeader />
       <div className="fixed md:w-64 hidden md:block ">
         <Sidebar />
       </div>
+      
     </div>
   );
 }
