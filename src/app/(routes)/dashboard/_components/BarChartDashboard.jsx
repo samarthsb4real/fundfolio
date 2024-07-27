6a32977c3666b31e5b1f1a1c3d156bef6300beb9
samarthsb4real <samarthbhadane23@gmail.com>
2024-07-28 00:32:47 +0530

    deployment fix#2

--- src/app/(routes)/dashboard/_components/BarChartDashboard.jsx
+++ src/app/(routes)/dashboard/_components/BarChartDashboard.jsx
@@ -11,7 +11,7 @@ import {
 
 export default function BarChartDashboard({ budgetList }) {
   return (
-    <div className="border rounded-xl p-6">
+    <div className="border rounded-xl p-6 shadow-md">
       <h2 className="font-bold text-lg">Activity</h2>
       <ResponsiveContainer width={"80%"} height={300}>
         <BarChart data={budgetList} margin={{ top: 7 }}>
