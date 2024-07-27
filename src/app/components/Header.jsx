c1312f300bb6e292faa12c043e3e9f13d8c9ae27
samarthsb4real <samarthbhadane23@gmail.com>
2024-07-27 10:07:40 +0530

    home page

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,15 +1,16 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Header() {
   return (
-    <section className="flex flex-row items-center justify-between">
-      <div className="flex items-center">
-        <Image src="/favicon.ico" width={40} height={40} />
-        <h1 className="text-2xl">FundFolio</h1>
+    <section className="flex flex-row items-center justify-between py-4 px-8 border-b">
+      <div className="flex items-center gap-2">
+        <Image src="/favicon.ico" width={30} height={40} />
+        <h1 className="text-2xl font-bold">FundFolio</h1>
       </div>
       <div className="flex flex-row gap-3">
-        <button>Dashboard</button>
-        <button>Get Started</button>
+        <Button variant="outline">Dashboard</Button>
+        <Button>Get started</Button>
       </div>
     </section>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 export default function Home() {
   return (
     <main>
-      <div className="p-8"><Header/></div>
+      <Header />
     </main>
   );
 }
